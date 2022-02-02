688cb6d83ce0843ea54d3ab397837dbe066c8621
carlh <cghinkle427@gmail.com>
2022-02-02 04:03:33 -0500

    Initial Commit

---
+++ TheTempoist/TheTempoistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheTempoistApp.swift
+//  TheTempoist
+//
+//  Created by Carl Hinkle on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct TheTempoistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
