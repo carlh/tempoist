cb77fd8ba41d4862b928efed219dc2cc33b3ab4f
carlh <cghinkle427@gmail.com>
2022-02-11 13:11:32 -0500

    Add "Other Apps" content.

--- TheTempoist/screens/InfoView/OtherAppsInfoView.swift
+++ TheTempoist/screens/InfoView/OtherAppsInfoView.swift
@@ -9,12 +9,47 @@ import SwiftUI
 
 struct OtherAppsInfoView: View {
     var body: some View {
-        Text("Other Apps")
+        ZStack {
+            Background()
+                .edgesIgnoringSafeArea(.all)
+            ScrollView {
+                VStack(spacing: 24) {
+                    Text("Tunelyzer")
+                        .font(.system(.largeTitle, design: .rounded))
+                    
+                    Image("TunelyzerIcon")
+                        .resizable()
+                        .frame(width: 167, height: 167)
+                        .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                    
+                    Text("A tuner with built in presets for all of your common guitar tunings. There's also a chromatic tuner so that it's easy to get your guitar, bass, banjo, or ukelele in perfect tune.  ")
+                        .multilineTextAlignment(.leading)
+                    
+                    Button {
+                        guard let tunelyzerURL = URL(string: "https://apps.apple.com/app/id1440592987")
+                        else { return }
+                        Task {
+                            await UIApplication.shared.open(tunelyzerURL, options: [:])
+                        }
+                    } label: {
+                        Text("Open in app store")
+                    }
+                    .buttonStyle(.bordered)
+                }
+                .padding()
+            }
+        }
+        .navigationTitle("My apps")
     }
 }
 
 struct OtherAppsInfoView_Previews: PreviewProvider {
     static var previews: some View {
-        OtherAppsInfoView()
+        NavigationView {
+            OtherAppsInfoView()
+                .preferredColorScheme(.dark)
+                .navigationBarTitleDisplayMode(.inline)
+        }
+        
     }
 }
